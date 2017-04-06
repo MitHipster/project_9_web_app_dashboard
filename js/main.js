dd072c54ad9f091c4323efaf7d35236dde10932a
Tim Acker <tjacker31@gmail.com>
2017-04-05 20:59:02 -0400

    Added Menu Icon and Refactored Header
    Added a menu icon that will show and hide the navigation bar on smaller
    screens. Refactored Sass for the header to read more logically and to
    reduce nesting. Created a mixing to style icon links.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 /*jslint esversion: 6, browser: true*/
 /*global window, $, jQuery, alert*/
 
+document.getElementById('arrow').addEventListener('click', function() {
+  this.classList.toggle('rotate');
+});
