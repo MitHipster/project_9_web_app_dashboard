8cc738c148ef71bcc666ebb86573bb008aaf1919
Tim Acker <tjacker31@gmail.com>
2017-04-05 23:54:07 -0400

    Off-Canvas Menu
    Added functionality to show and hide navigation menu. Will eventually
    be available only on screen sizes under 768px.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 /*jslint esversion: 6, browser: true*/
 /*global window, $, jQuery, alert*/
 
-document.getElementById('arrow').addEventListener('click', function() {
-  this.classList.toggle('rotate');
+$(document).ready( () => {
+  $('#arrow').on('click', () => {
+    $('main').toggleClass('slide-right');
+    $('#arrow').toggleClass('rotate');
+  });
 });
