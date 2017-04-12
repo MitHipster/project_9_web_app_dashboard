ac0a6d2d246d9b37ecf80cf35c2ca21448db10a0
Tim Acker <tjacker31@gmail.com>
2017-04-12 11:22:13 -0400

    Line Chart Datasets
    Created additional datasets for Hourly, Daily, and Monthly for use with
    the timescale buttons. Update functionality is not currently enabled.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ $(document).ready( () => {
     $('main').toggleClass('slide-right');
     $('#arrow').toggleClass('rotate');
   });
+  
+  // On click event for line chart buttons to switch labels and datasets based timescale selected
+  $('.line-chart-timescale').on('click', 'button', function () {
+    lineTimescale = $(this).attr('value');
+    lineChart.update();
+  });
 });
 
 // Global chart defaults
@@ -27,20 +33,46 @@ Chart.defaults.scale.ticks.beginAtZero = true;
 
 Chart.defaults.global.title.display = true;
 Chart.defaults.global.legend.display = false;
-//Chart.defaults.global.tooltips.backgroundColor = '#fff';
 
-//                         
+// Line chart variables
 const LINE_CHART = $('#line-chart');
 const lineFill = 'rgba(62, 92, 118, 0.4)'; // $deep-space (#3e5c76)
 const lineBorder = colorMain;
 
+const lineLabels = {
+  hourly: ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"],
+  daily: ["W", "Th", "F", "Sat", "Sun", "M", "Tu", "W", "Th", "F", "Sat"],
+  weekly: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+  monthly: ["Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May"]
+};
+const lineData = {
+  hourly: [25, 30, 15, 35, 20, 45, 20, 15, 35, 15, 20],
+  daily: [225, 350, 300, 150, 100, 450, 300, 250, 400, 350, 200],
+  weekly: [500, 1000, 750, 1250, 1750, 1250, 1000, 1500, 2000, 1500, 2000],
+  monthly: [6000, 7250, 6000, 6500, 5750, 4250, 4000, 3500, 4500, 7250, 8000]
+};
+const lineTicksMax = {
+  hourly: 50,
+  daily: 500,
+  weekly: 2500,
+  monthly: 8750
+};
+const lineTicksStepSize = {
+  hourly: 10,
+  daily: 100,
+  weekly: 500,
+  monthly: 1750
+};
+
+var lineTimescale = 'weekly';
+
 let lineChart = new Chart(LINE_CHART, {
   type: 'line',
   data: {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    labels: lineLabels[lineTimescale],
     datasets: [
       {
-        data: [500, 1000, 750, 1250, 1750, 1250, 1000, 1500, 2000, 1500, 2000],
+        data: lineData[lineTimescale],
         fill: true,
         lineTension: 0,
         backgroundColor: lineFill,
@@ -62,8 +94,8 @@ let lineChart = new Chart(LINE_CHART, {
     scales: {
       yAxes: [{
         ticks: {
-          max: 2500,
-          stepSize: 500,
+          max: lineTicksMax[lineTimescale],
+          stepSize: lineTicksStepSize[lineTimescale],
         }
       }]
     }
